a034d22223d16ffd7b1fe62e4cfb42e78805a1ef
Boomstdev <68290796+Boomstdev@users.noreply.github.com>
2021-02-04 23:12:23 +0700

    Add files via upload
    build rotate page and smoot page indicator

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -3,43 +3,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'dart:ui';
 import 'dart:convert';
+import 'dart:math';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({this.data});
+
   final data;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
-var rotate;
+  var rotate;
+  final _controller = new PageController();
+
   @override
   void initState() {
     super.initState();
-// var data1 = jsonDecode(widget.data)[0]['movies'][0]['Title'];
-//     print(widget.data);
-//     Map<String, dynamic> rotate = jsonDecode(widget.data)[0]['movies'][0];
-    // List rotage1 = jsonDecode(widget.data)[0]['movies'];
-    // Map<String , dynamic> avent = rotage1[5];
-    // print('debug');
-    // print(data1);
-    // print('debug0');
-    // print(rotage1[0]['Title']);
-    // print('debug1');
-    // print(avent['Poster']);
-    // updateMovie(rotage1);
+
     updateMovie(widget.data);
     print(rotate[0]['Title']);
+    print('debug');
+    print(rotate.length);
+    print(rotate[0]['Poster']);
   }
-  void updateMovie(dynamic dataRotate){
+
+  void updateMovie(dynamic dataRotate) {
     rotate = jsonDecode(dataRotate)[0]['movies'];
     return rotate;
   }
+
   // Map<String, dynamic> movie1 = jsonDecode(data)[0]['movies'][0]['Title'];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.white70,
         appBar: AppBar(
           title: Text('Netflax'),
           centerTitle: true,
@@ -52,8 +54,57 @@ var rotate;
             children: <Widget>[
               Text(
                 // movie1
-                 'Rotate',
+                'Rotate',
                 style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 490.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 450.0,
+                      child: PageView.builder(
+                        physics: new AlwaysScrollableScrollPhysics(),
+                        controller: _controller,
+                        itemCount: rotate.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return SizedBox(
+                            height: 450.0,
+                            child: Card(
+                              margin: EdgeInsets.all(10.0),
+                              child: Image.network(
+                                  rotate[index]['Poster']),
+                            ),
+                          ); //Poster[index % _pages.length];
+                        },
+                      ),
+                    ),
+                    // Container(
+                    //   height: 50.0,
+                    //   color: Colors.white12,
+                    //   padding: const EdgeInsets.all(20.0),
+                    //   child: new Center(
+                    //     child: new DotsIndicator(
+                    //       controller: _controller,
+                    //       itemCount: rotate.length,
+                    //       onPageSelected: (int index) {
+                    //         _controller.animateToPage(
+                    //           index,
+                    //           duration: Duration(milliseconds: 500),
+                    //           curve: Curves.ease,
+                    //         );
+                    //       },
+                    //     ),
+                    //   ),
+                    // ),
+                    SmoothPageIndicator(
+                        controller: _controller,
+                        count: rotate.length,
+                    effect: WormEffect(),
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 height: 200.0,
@@ -64,10 +115,11 @@ var rotate;
                   itemCount: 5,
                   itemBuilder: (BuildContext context, int index) => Card(
                     child: Center(
-                      child: Image.network(rotate[index]['Poster']),),
+                      child: Image.network(rotate[index]['Poster']),
                     ),
                   ),
                 ),
+              ),
               Text(
                 'Big Slide',
                 style: TextStyle(fontSize: 18),
@@ -132,59 +184,39 @@ var rotate;
                   ),
                 ),
               ),
-             Text('Small'),
-             SizedBox(
-               height: 200.0,
-               width: 200.0,
-               child: GridView.count(
-                 primary: false,
-                 padding: const EdgeInsets.all(20),
-                 crossAxisSpacing: 3,
-                 mainAxisSpacing: 3,
-                 crossAxisCount: 2,
-                 children: <Widget>[
-                   Container(
-                     padding: const EdgeInsets.all(8),
-                     child: const Text("He'd have you all unravel at the"),
-                     color: Colors.teal[100],
-                   ),
-                   Container(
-                     padding: const EdgeInsets.all(8),
-                     child: const Text('Heed not the rabble'),
-                     color: Colors.teal[200],
-                   ),
-                   Container(
-                     padding: const EdgeInsets.all(8),
-                     child: const Text('Sound of screams but the'),
-                     color: Colors.teal[300],
-                   ),
-                   Container(
-                     padding: const EdgeInsets.all(8),
-                     child: const Text('Who scream'),
-                     color: Colors.teal[400],
-                   ),
-
-                 ],
-               )
-             ),
-            //   Column(
-            //     children: [
-            //       GridView.count(
-            //         primary: false,
-            //         padding: const EdgeInsets.all(20),
-            //         crossAxisSpacing: 10,
-            //         mainAxisSpacing: 10,
-            //         crossAxisCount: 1,
-            //         children:[
-            //           Container(
-            //             padding: const EdgeInsets.all(8),
-            //             child: const Text("He'd have you all unravel at the"),
-            //             color: Colors.teal[100],
-            //           ),
-            //           ],
-            //   )
-            // ],
-            //   ),
+              Text('Small'),
+              SizedBox(
+                  height: 200.0,
+                  width: 200.0,
+                  child: GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 3,
+                    mainAxisSpacing: 3,
+                    crossAxisCount: 2,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text("He'd have you all unravel at the"),
+                        color: Colors.teal[100],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text('Heed not the rabble'),
+                        color: Colors.teal[200],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text('Sound of screams but the'),
+                        color: Colors.teal[300],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text('Who scream'),
+                        color: Colors.teal[400],
+                      ),
+                    ],
+                  )),
             ],
           ),
         ),
